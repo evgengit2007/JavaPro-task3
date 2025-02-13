9e8f19f6b23b904ed2adb0a7d761424a4f637ef3
evgengit2007 <166688467+evgengit2007@users.noreply.github.com>
2025-02-13 18:39:35 +0700

    На проверку

--- src/mian/java/ru/vtb/javaPro/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.vtb.javaPro;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/mian/java/ru/vtb/javaPro/MainThread.java
@@ -0,0 +1,26 @@
+package ru.vtb.javaPro;
+
+public class MainThread {
+    public static void main(String[] args) throws InterruptedException{
+        CustomThreadPool customThreadPool = new CustomThreadPool(5);
+        System.out.println("----Потоки стартовали----");
+
+        for (int i = 0; i < 10; i++) {
+            int exec = i;
+            customThreadPool.execute(() -> {
+                System.out.println("Добавить задачу " + exec + " на исполнение");
+            });
+        }
+
+        Thread.sleep(2000);
+        customThreadPool.shutdown();
+
+        for (int i = 10; i < 20; i++) {
+            int exec = i;
+            customThreadPool.execute(() -> {
+                System.out.println("2 итерация. Добавить задачу " + exec + " на исполнение");
+            });
+        }
+//        customThreadPool.awaitTermination();
+    }
+}
